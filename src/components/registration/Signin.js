3025e0cd62decdd5bba9a18bb9f0d8d4dd2ddda6
kimberlykuya <kimberlykuya@gmail.com>
2022-07-07 16:19:39 +0300

    sign in update

--- src/components/registration/Signin.js
+++ src/components/registration/Signin.js
@@ -1,11 +1,11 @@
 export default function Signin(){
     return (
         <div>
-            <form>
+            ype="submit" className="register-button" value="LOG IN" />
+            </form><form>
             <input className="register-input" type="email" name="email" placeholder="Email Address" />
             <input className="register-input" type="password" name="password" placeholder="Password" />
-            <input type="submit" className="register-button" value="LOG IN" />
-            </form>
+            <input t
         </div>
     )
 }
