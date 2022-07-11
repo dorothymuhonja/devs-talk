8f85a460a95a77c357fc4a81c0c9715c09ee1ae8
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-07-11 14:31:46 +0300

    import tailwind to other css

--- src/components/registration/Navbar.js
+++ src/components/registration/Navbar.js
@@ -17,10 +17,10 @@ export default function Navbar(){
           <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
         </li>
         <li>
-          <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0    ">About</a>
+          <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</a>
         </li>
         <li>
-          <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0    ">Features</a>
+          <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Features</a>
         </li>
       </ul>
     </div>
