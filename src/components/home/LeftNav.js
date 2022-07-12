ed32e9818efad1f081685f6b25f67dd1392bbd35
munenepeter <peternjega7@gmail.com>
2022-07-12 14:26:45 +0300

    catching up with @abzed

--- src/components/home/LeftNav.js
+++ src/components/home/LeftNav.js
@@ -7,16 +7,16 @@ export default function LeftNav() {
         <i className="fas fa-home material-icons"></i>
         </a>
         <a  href="somewhere" className="menu__item " data-tooltip="Online Users">
-        <i class="fas fa-users material-icons"></i>
+        <i className="fas fa-users material-icons"></i>
         </a>
         <a  href="somewhere" className="menu__item" data-tooltip="Profile">
-        <i class="fas fa-user-circle material-icons"></i>
+        <i className="fas fa-user-circle material-icons"></i>
         </a>
         <a  href="somewhere" className="menu__item" data-tooltip="Account Security">
-        <i class="fas fa-lock material-icons"></i>
+        <i className="fas fa-lock material-icons"></i>
         </a>
         <a  href="somewhere" className="menu__item" data-tooltip="Settings">
-        <i class="fas fa-sliders-h material-icons"></i>
+        <i className="fas fa-sliders-h material-icons"></i>
         </a>
       </nav>
         </div>
--- src/components/registration/Navbar.js
+++ src/components/registration/Navbar.js
@@ -3,30 +3,24 @@ import { Link } from "react-router-dom";
 export default function Navbar(){
     return (
         
-<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
-  <div className="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="somewhere" className="flex items-center">
-        <span className="self-center text-xl font-semibold whitespace-nowrap ">Devs Talk</span>
-    </a>
-    <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200   " aria-controls="mobile-menu" aria-expanded="false">
-      <span className="sr-only">Open main menu</span>
-      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-      <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-      <ul className="d-flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-        <li>
-          <a href="somewhere" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="somewhere" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</a>
-        </li>
-        <li>
-          <a href="somewhere" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Features</a>
-        </li>
-        <Link to="/welcome">About</Link>
-      </ul>
-    </div>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a className="navbar-brand" href="#">Hidden brand</a>
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    
   </div>
 </nav>
 
