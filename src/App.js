9b83379d1dc7bda59af679a80d63cf778b8237bf
bewld-devs <bewld.devs@gmail.com>
2022-07-21 22:40:30 +0300

    add settings folder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Feeds from './components/home/Feeds';
 import Navbar from './components/registration/Navbar';
 import Welcome from './components/registration/Welcome'
+import Settings from './components/settings/Settings';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Feeds />} />
         <Route exact path="/welcome" element={<Welcome />} />
+        <Route exact path="/settings" element={<Settings />} />
       </Routes>
     </>
     
