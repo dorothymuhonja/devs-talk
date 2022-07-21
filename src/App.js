a175e6949ac406445bc511720b1bdbbfad1a4d19
kimberlykuya <kimberlykuya@gmail.com>
2022-07-21 22:55:09 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Feeds from './components/home/Feeds';
-import Navbar from './components/registration/Navbar';
-import Welcome from './components/registration/Welcome'
+import Feeds from "./components/home/Feeds";
+import Navbar from "./components/registration/Navbar";
+import Welcome from "./components/registration/Welcome";
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Feeds />} />
         <Route exact path="/welcome" element={<Welcome />} />
       </Routes>
     </>
-    
   );
 }
 
--- src/components/registration/Welcome.js
+++ src/components/registration/Welcome.js
@@ -1,48 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Signin from "./Signin";
 import Signup from "./Signup";
 
-export default function Welcome(){
+export default function Welcome() {
+  const [authType, setAuthType] = useState(false);
 
-    const [authType, setAuthType] = useState(false)
-
-    function changeAuthType(){
-        setAuthType(authType => !authType)
-    }
-    return (
-        <>
-        <div className="welcome">
-            <div className="welcome-menu">
-                <div className="col-lg-1"><div className="welcome-round-logo"></div>
-                
+  function changeAuthType() {
+    setAuthType((authType) => !authType);
+  }
+  return (
+    <>
+      <div className="welcome">
+        <div className="welcome-menu">
+          <div className="col-lg-1">
+            <div className="welcome-round-logo"></div>
+          </div>
+          <h1 className="subtitle">DEV TALKS</h1>
+        </div>
+        <div className="welcome-body">
+          <div className="left col-lg-7">
+            <div className="welcome-image"></div>
+            <h1 className="title text-blue-500">Be Collaborative</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed
+              molestie ipsum.
+            </p>
+          </div>
+          <div className=" col-lg-5">
+            <div className="welcome-auth-view">
+              <div className="tab">
+                <div id="sign-in-corner" className="tab-bar">
+                  <button className="text-center" onClick={changeAuthType}>
+                    Sign In
+                  </button>
                 </div>
-                <h1 className="subtitle">DEV TALKS</h1>
-            </div>
-            <div className="welcome-body">
-                <div className="left col-lg-7">
-                <div className="welcome-image">
+                <div id="register-corner" className="tab-bar center-block">
+                  <button onClick={changeAuthType}>Register</button>
                 </div>
-                <h1 className="title text-blue-500">Be Collaborative</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed molestie ipsum.</p>
-                </div>
-                <div className=" col-lg-5">
-                <div className="welcome-auth-view">
-                    <div className="tab">
-                        <div id="sign-in-corner" className="tab-bar"><button className="text-center" onClick={changeAuthType}>Sign In</button></div>
-                        <div id="register-corner" className="tab-bar center-block"><button onClick={changeAuthType}>Register</button></div>
-                    </div>
-                    {/* <div className="welcome-auth-buttons">
+              </div>
+              {/* <div className="welcome-auth-buttons">
                         <button onClick={changeAuthType} className="btn" type="button">Sign In</button>
                         <button onClick={changeAuthType} className="btn" type="button">Register</button>
                     </div> 
                     <hr /> */}
-                    <div className="welcome-auth-inner-view">
-                        {authType? <Signup /> : <Signin />}
-                    </div>
-                </div>
-                </div>
+              <div className="welcome-auth-inner-view">
+                {authType ? <Signup /> : <Signin />}
+              </div>
             </div>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
